d592de1da1d55cd655c5a92a7eef71b1ebfa5741
Okan Sağlam <okan415461@gmail.com>
2024-10-31 20:10:23 +0300

    Update icmp_receiver.py

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -3,4 +3,4 @@ import scapy.all as scapy
 
 
 # Implement your ICMP receiver here
-# scapy.sniff(count = 1, filter = "icmp and host sender", prn = lambda x: x.show())
+scapy.sniff(count = 1, filter = "icmp and host sender", prn = lambda x: x.show())
